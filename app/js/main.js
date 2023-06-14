17dcdb26154d2bbf7cd54203109c0a83ee08e101
mihailmotorkin <motorkinmihail@gmail.com>
2023-06-14 19:45:35 +0800

    add product page

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,50 @@ $(function(){
     slidesToScroll: 3
   });
 
-  $('.questions__item-btn').on('click', function(){
-    // $(this).removeClass('active');
-    // $(this).toggleClass('active');
 
-    // $('.questions__item-btn').toggleClass('questions__item-btn--active');
-    
-    
-    $('.questions__item-btn').removeClass('questions__item-btn--active'); 
-    $(this).addClass('questions__item-btn--active');
-    
+  $('.questions__item-btn').on('click', function() { 
+
+    let $p = $(this).closest('li').find('p.questions__item-text');
+    $p.toggleClass('questions__item-text--active');
+
   });
 
+
+  $('.product-slider__big').slick({
+    asNavFor: '.product-slider__small',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    fade: true
+  });
+  $('.product-slider__small').slick({
+    asNavFor: '.product-slider__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: false
+  });
+
+
+  $('.product-item__choice-angle').on('click', function() { 
+
+    let $div = $(this).closest('.product-item__choice-inner').find('.product-item__choice');
+    $div.toggleClass('product-item__choice--active');
+
+  });
+
+  $('.product-item__choice-angle').on('click', function () {
+    $(this).toggleClass('product-item__choice-angle--active');
+  });
+
+
+  $('.product-tabs__top-item').on('click', function(e){
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
 
 })
